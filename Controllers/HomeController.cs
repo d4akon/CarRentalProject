dbd88932cac20a01d25e4dc0856c73ed6f331af5
Szymon Maj <szymon.maj@icloud.com>
2023-02-12 21:45:38 +0100

    Changes to the all views

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,5 +1,7 @@
-﻿using CarRentalProject.Models;
+﻿using CarRentalProject.Data;
+using CarRentalProject.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Diagnostics;
 
 namespace CarRentalProject.Controllers
@@ -8,19 +10,19 @@ namespace CarRentalProject.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _context;
+
+        public HomeController(ILogger<HomeController> logger, ApplicationDbContext context)
         {
             _logger = logger;
+            _context = context;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
+            return _context.Cars != null ?
+                         View(await _context.Cars.ToListAsync()) :
+                         Problem("Entity set 'ApplicationDbContext.Cars'  is null.");
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
