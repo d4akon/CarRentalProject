2dcb843d2d9f15981a57d262a0f5afc571732325
Szymon Maj <szymon.maj@icloud.com>
2023-02-12 20:37:31 +0100

    Created connection to the database and create table Cars

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CarRentalProject.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarRentalProject.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
