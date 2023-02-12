4eefa00d4eb27d3b002f4a81b00ac547a6c5acfb
Szymon Maj <szymon.maj@icloud.com>
2023-02-12 23:19:10 +0100

    Added todo reservation model

---
+++ Models/Reservation.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CarRentalProject.Models
+{
+    public class Reservation
+    {
+        //TODO finish this model
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public DateTime DateOfRental { get; set; }
+        public DateTime DateOfReturn { get; set; }
+        public int? RentalTime => (DateOfReturn - DateOfRental).Days;
+        public ReservationStatus Status { get; set; }
+
+        //public Customer Customer { get; set; }
+        //public Employee Employee { get; set; }
+        public Car Car { get; set; }        
+    }
+
+    public enum ReservationStatus
+    {
+        Unknown,
+        [Display(Name = "In Progress")]
+        InProgress,
+        Finished
+    }
+}
