c6125a5a9f7cecbb37c46160ea9ffc5cc8afa9a4
Szymon Maj <szymon.maj@icloud.com>
2023-02-12 20:25:43 +0100

    Created Car model

---
+++ Models/Car.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace CarRentalProject.Models;
+
+public class Car
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public CarTypes Type { get; set; }
+    [StringLength(50)]
+    [Required]
+    public string Brand { get; set; }
+    [StringLength(50)]
+    [Required]
+    public string Model { get; set; }
+    [StringLength(50)]
+    [Required]
+    public string Color { get; set; }
+    [Url]
+    public string ImageUrl { get; set; }
+}
+
+public enum CarTypes
+{
+    Coupe,
+    Crossover,
+    Hatchback,
+    Pickup,
+    Sedan,
+    [Display(Name = "Sports Car")]
+    SportsCar,
+    SUV
+}
